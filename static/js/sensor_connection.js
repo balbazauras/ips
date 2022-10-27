ccb3f1b0aff11b680ff5ff5bd01a9ef81ae98d36
balbazauras <staugaitis.liudas@gmail.com>
2022-10-27 10:30:30 +0300

    Changes to template

--- static/js/sensor_connection.js
+++ static/js/sensor_connection.js
@@ -1,10 +1,15 @@
+$("#sensorConnect").bootstrapSwitch({
+  onSwitchChange: function (e, state) {
+    alert(state);
+  },
+});
 function waitForSocketConnection(socket) {
   setTimeout(function () {
     if (socket.readyState === 0) {
       document.getElementById("status").textContent = "CONNECTING";
       waitForSocketConnection(socket);
     } else if (socket.readyState === 1) {
-      document.getElementById("status").textContent = "OPEN";
+      document.getElementById("status").textContent = "CONNECTED";
       waitForSocketConnection(socket);
     } else if (socket.readyState === 2) {
       document.getElementById("status").textContent = "CLOSING";
@@ -15,6 +20,7 @@ function waitForSocketConnection(socket) {
     }
   }, checkConInterval);
 }
+
 function sendToSensor() {
   if (typeof socket === "undefined") {
     alert("You have to connect to sensor");
@@ -31,7 +37,7 @@ function sendToSensor() {
 var checkConInterval = 100; //time interval between checks for websocket connection
 var socket;
 function init() {
-  socket = new WebSocket("ws://" + "192.168.205.85" + ":81/");
+  socket = new WebSocket("ws://" + "192.168.2.127" + ":81/");
   waitForSocketConnection(socket);
   socket.onmessage = function (event) {
     processCommand(event);
