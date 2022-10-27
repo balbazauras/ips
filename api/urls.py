1d9391755e3c9be84b208226c8e4724f909729bb
balbazauras <staugaitis.liudas@gmail.com>
2022-10-27 10:28:05 +0300

    Added new enppoint url

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ app_name = 'api'
 urlpatterns = [
     path('', views.sensor_data, name="get_data"),
     path('get-all/', views.get_all_sensor_data, name="get_all_data"),
+    path('get-params/', views.get_sensor_params, name="get_sensor_param"),
 ]
